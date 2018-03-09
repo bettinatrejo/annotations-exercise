c20d65bfa6b9b08e1fbea64ca04090d72e934b22
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-09 12:19:55 -0600

    added suppressWarnings

--- src/Author.java
+++ src/Author.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-@SuppressWarnings("unchecked")
-
 public class Author extends Person {
     private List books;
 
@@ -19,14 +17,17 @@ public class Author extends Person {
      * @deprecated Use publishedBooks instead
      */
     @Deprecated
+    @SuppressWarnings("unchecked")
     public List<String> getBooks() {
         return books;
     }
 
+    @SuppressWarnings("unchecked")
     public List<String> publishedBooks() {
         return books;
     }
 
+    @SuppressWarnings("unchecked")
     public void addBook(String book) {
         books.add(book);
     }
